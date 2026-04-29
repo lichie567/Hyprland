cf438e2b347e3ff7b020afcfff6470c0381691f3
lichie <lichie567@protonmail.com>
2026-04-29 04:51:13 -0700

    add window rule parsing for no mouse

--- src/desktop/rule/windowRule/WindowRule.cpp
+++ src/desktop/rule/windowRule/WindowRule.cpp
@@ -249,6 +249,8 @@ static std::expected<WindowRuleEffectValue, std::string> parseWindowRuleEffect(C
         case WINDOW_RULE_EFFECT_NO_SCREEN_SHARE:
         case WINDOW_RULE_EFFECT_NO_VRR:
         case WINDOW_RULE_EFFECT_CONFINE_POINTER:
+        case WINDOW_RULE_EFFECT_NOMOUSEMOVE:
+        case WINDOW_RULE_EFFECT_NOMOUSERESIZE:
         case WINDOW_RULE_EFFECT_STAY_FOCUSED: return truthy(raw);
 
         case WINDOW_RULE_EFFECT_FULLSCREENSTATE: {
